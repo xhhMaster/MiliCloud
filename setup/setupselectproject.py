52be53011292466c51785f42786a13a8bac2cdef
HH890612 <752247485@qq.com>
2015-12-15 17:16:07 +0800

    修正了参数传递

--- setup/setupselectproject.py
+++ setup/setupselectproject.py
@@ -18,15 +18,20 @@ class Widget(QtGui.QWidget, Ui_Widget):
         
         #获取当前选中行
         selectedIndex = self.projectList.currentIndex().row()
-       
-        if selectedIndex == -1:
-            cmds.confirmDialog(b=u"确定",m=u"请选择一个项目！",t=u"提示信息")
-        else:
-            selectedContent = self.projectList.currentIndex().data()
-            print self.contents.find(selectedContent)
+
+        if selectedIndex != -1:
+            selectedRow = self.projectList.currentItem().row()
+            selectedId = self.projectList.item(selectedRow,0).text()
+            selectedContent = self.projectList.item(selectedRow,1).text()
+            selectedDesc = self.projectList.item(selectedRow,2).text()
+            if selectedDesc == "":
+                selectedDesc = u"暂无内容"           
             import setup.setupselectworkfiles as setupselectworkfiles
-            self.Widget = setupselectworkfiles.Widget(content = selectedContent)
+            self.Widget = setupselectworkfiles.Widget(pid = selectedId,content = selectedContent,desc = selectedDesc )
             self.Widget.show()        
+           
+        else:
+            cmds.confirmDialog(b=u"确定",m=u"请选择一个项目！",t=u"提示信息")
                  
     #点击取消按钮触发的事件    
     def cancelClicked(self):
@@ -39,19 +44,27 @@ class Widget(QtGui.QWidget, Ui_Widget):
         
         #获取项目数据
         contents = projectservice.Project().callService()
-             
-        #创建一个容器列表存放数据
-        itemList = QtGui.QStandardItemModel()
-        for content in contents:        
-            item  = QtGui.QStandardItem(content['NAME'])
-            itemList.appendRow(item)
-           
-        self.projectList.setModel(itemList)
-        
-        #让list默认不选中
-        index =  QtCore.QModelIndex()
-        self.projectList.setCurrentIndex(index)
-    
-
        
-
+        self.projectList.setColumnCount(3)
+        self.projectList.setRowCount(int(len(contents)))
+        header = ['projectID','projectName','description']
+        self.projectList.setHorizontalHeaderLabels(header)
+       
+        for index,content in enumerate(contents):
+            itemId = QtGui.QTableWidgetItem(content['id'])
+            itemName = QtGui.QTableWidgetItem(content['name'])
+            itemDesc = QtGui.QTableWidgetItem(content['description'])
+            self.projectList.setItem(index,0,itemId)
+            self.projectList.setItem(index,1,itemName)
+            self.projectList.setItem(index,2,itemDesc)      
+             
+        self.projectList.setColumnHidden(0,True)
+        self.projectList.setColumnHidden(2,True)
+        
+        index = QtCore.QModelIndex()
+        self.projectList.setCurrentIndex(index)
+       
+                 
+        
+      
+    
--- setup/setupselectworkfiles.py
+++ setup/setupselectworkfiles.py
@@ -1,15 +1,19 @@
 # -*- coding: utf-8 -*-
-from PySide import QtGui
+from PySide import QtGui,QtCore
 from ui.selectworkfiles_ui import Ui_Widget
 
 class Widget(QtGui.QWidget, Ui_Widget):
-    def __init__(self,content,parent=None):
+    def __init__(self,pid,desc,content,parent=None):
         super(Widget,self).__init__(parent)
+        self.pid = pid
+        self.desc = desc
+        self.content = content
         self.setupUi(self)
-        self.getShotAndAssetInfo()
-        self.getTaskInfo()
-        self.projectNameEdit.setText(content)
-
+        self.getShotAndAssetInfo(self.pid)
+        self.projectNameEdit.setText(self.content)
+        self.projectDescEdit.setText(self.desc)
+        
+        self.List.clicked.connect(self.getTaskInfo)
         self.createBtn.clicked.connect(self.createClicked)
         self.cancelBtn.clicked.connect(self.cancelClicked)
         
@@ -26,35 +30,80 @@ class Widget(QtGui.QWidget, Ui_Widget):
 
         
         
-    def getShotAndAssetInfo(self):  
+    def getShotAndAssetInfo(self,pid):  
         #导入ShotService
         import service.shotservice as shotservice
-        shotContents = shotservice.Shot().callService(2)
+        shotContents = shotservice.Shot().callService(pid)
         
         #导入AssetService
         import service.assetservice as assetservice
-        assertContents = assetservice.Asset().callService(2)
+        assetContents = assetservice.Asset().callService(pid)
                      
         #创建一个容器列表存放数据
-        List=QtGui.QListWidget()
-        for content in shotContents:        
-            QtGui.QListWidgetItem(content['name'],List)
+        self.List = self.__initTableWidget()
+
+        header = ['A&SID','A&SName','A&SType']
+        self.List.setHorizontalHeaderLabels(header)
+                               
+        if len(shotContents) > 0:
+            for index,content in enumerate(shotContents):
+                self.List.insertRow(index)    
+                itemId = QtGui.QTableWidgetItem(content['id'])
+                itemName = QtGui.QTableWidgetItem(content['name'])
+                itemType = QtGui.QTableWidgetItem("Shot")
+                self.List.setItem(index,0,itemId)
+                self.List.setItem(index,1,itemName)
+                self.List.setItem(index,2,itemType)  
         
-        for content in assertContents:        
-            QtGui.QListWidgetItem(content['name'],List)
+        if len(assetContents) > 0:
+            for index,content in enumerate(assetContents):
+                self.List.insertRow(index)
+                itemId = QtGui.QTableWidgetItem(content['id'])
+                itemName = QtGui.QTableWidgetItem(content['name'])
+                itemType = QtGui.QTableWidgetItem("Asset")
+                self.List.setItem(index,0,itemId)
+                self.List.setItem(index,1,itemName)
+                self.List.setItem(index,2,itemType)    
+                  
+        self.List.setColumnHidden(0,True)
+        self.List.setColumnHidden(2,True)
+        index = QtCore.QModelIndex()
+        self.List.setCurrentIndex(index)
             
-        self.shotScrollArea.setWidget(List)
+        self.shotScrollArea.setWidget(self.List)
         
-    
+       
+      
     def getTaskInfo(self):
+        
+        selectedRow = self.List.currentItem().row()
+        selectedId = self.List.item(selectedRow,0).text()
+        selectedType = self.List.item(selectedRow,2).text()
         
         #导入apiService
         import service.taskservice as taskservice
-        contents = taskservice.Task().callService(2)
+        contents = taskservice.Task().callService(selectedId,selectedType)
         
         #创建一个容器列表存放数据
         List=QtGui.QListWidget()
-        for content in contents:        
-            QtGui.QListWidgetItem(content['content'],List)
+        
+        if len(contents) > 0:
+            for content in contents:        
+                QtGui.QListWidgetItem(content['content'],List)
           
         self.tasksScrollArea.setWidget(List)
+
+
+    def __initTableWidget(self):
+        List = QtGui.QTableWidget()
+        List.setColumnCount(3)
+        List.verticalHeader().setVisible(False)
+        List.horizontalHeader().setVisible(False)
+        List.setFrameShape(QtGui.QFrame.NoFrame)
+        List.setSelectionMode(QtGui.QAbstractItemView.SingleSelection)
+        List.setShowGrid(False)
+        List.setSelectionBehavior(QtGui.QAbstractItemView.SelectRows)
+        List.setEditTriggers(QtGui.QAbstractItemView.NoEditTriggers)
+        return List
+    
+    

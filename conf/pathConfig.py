743779f5edad7e41e238f199358cc82b1364567a
HH890612 <752247485@qq.com>
2016-02-03 16:40:34 +0800

    添加文件

---
+++ conf/pathConfig.py
@@ -0,0 +1,6 @@
+iconForSearch = 'C:/Users/HH.Pic-p-127/Documents/maya/scripts/Image/search.png' 
+iconForScreenBtn = 'C:/Users/HH.Pic-p-127/Documents/maya/scripts/Image/camera.png' 
+iconForCursor = 'C:/Users/HH.Pic-p-127/Documents/maya/scripts/Image/pointer.png'
+
+publishFilePath ='MayaPushFile/'
+publishImgPath = 'MayaPushImage/'
